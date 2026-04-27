c1aa3aa0da79f8474991a46c6f69202509e0054d
Samia Asmar <asmarsamia2003@gmail.com>
2026-04-28 01:03:06 +0300

    solve build errors

--- app/(home)/hotels/[hotelId]/components/CustomerReservationManagementSection.tsx
+++ app/(home)/hotels/[hotelId]/components/CustomerReservationManagementSection.tsx
@@ -24,11 +24,9 @@ import { useReservationFeedback } from '../hooks/useReservationFeedback'
 import {
   calculateCancellationFee,
   canModifyReservation,
-  formatPolicyPercentage,
   getFreeCancellationDeadline,
   getReservationEditDeadline,
   isFreeCancellationEligible,
-  RESERVATION_POLICY,
 } from '../utils/customerReservationPolicy'
 import { buildReservationContract } from '../utils/buildReservationContract'
 import { formatBookingDate, formatCurrency, getStayLength } from '../utils/roomBooking'
@@ -150,15 +148,6 @@ export function CustomerReservationManagementSection({
   )
   const cancellationFeeLabel = formatCurrencyValue(
     currentReservationCancellationFee,
-    currentReservation.currency
-  )
-  const cancellationFeeRateLabel = formatPolicyPercentage(RESERVATION_POLICY.cancellationFeeRate)
-  const reservationTotalLabel = formatCurrencyValue(
-    currentReservation.totalPrice,
-    currentReservation.currency
-  )
-  const refundAmountLabel = formatCurrencyValue(
-    Math.max(currentReservation.totalPrice - currentReservationCancellationFee, 0),
     currentReservation.currency
   )
   const roomTypeNameById = new Map(roomTypes.map(roomType => [String(roomType.id), roomType.name]))
@@ -315,7 +304,6 @@ export function CustomerReservationManagementSection({
                   freeCancellation={currentReservationFreeCancellation}
                   modificationDeadlineLabel={modificationDeadlineLabel}
                   freeCancellationDeadlineLabel={freeCancellationDeadlineLabel}
-                  cancellationFeeRateLabel={cancellationFeeRateLabel}
                   cancellationFeeLabel={cancellationFeeLabel}
                 />
 
@@ -384,10 +372,7 @@ export function CustomerReservationManagementSection({
         open={cancellation.cancelOpen}
         freeCancellation={currentReservationFreeCancellation}
         freeCancellationDeadlineLabel={freeCancellationDeadlineLabel}
-        reservationTotalLabel={reservationTotalLabel}
-        cancellationFeeRateLabel={cancellationFeeRateLabel}
         cancellationFeeLabel={cancellationFeeLabel}
-        refundAmountLabel={refundAmountLabel}
         isBusy={isBusy}
         onClose={cancellation.closeCancel}
         onConfirm={cancellation.confirmCancel}
