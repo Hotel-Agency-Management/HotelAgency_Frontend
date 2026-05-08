9106e0dc79a1be1897abf5aedc50889c58dfcd36
Samia Asmar <asmarsamia2003@gmail.com>
2026-05-09 00:36:25 +0300

    fix: resolve merge conflicts in homePageFactory

--- app/(home)/home/factories/homePageFactory.tsx
+++ app/(home)/home/factories/homePageFactory.tsx
@@ -2,21 +2,15 @@ import type { ComponentType } from 'react'
 import { USER_ROLES, type UserRole } from '@/lib/abilities'
 import { CustomerHotelsPage } from '../../hotels/components/CustomerHotelsPage'
 import SuperAdminDashboardPage from '../../admin-dashboard/page'
-<<<<<<< support-property-manager-dashboard
 import PropertyManagerDashboardPage from '../../property-manager-dashboard/page'
-=======
 import FrontDeskDashboardPage from '../../front-desk-dashboard/page'
->>>>>>> main
 
 type HomePageComponent = ComponentType
 
 const roleHomePageMap: Partial<Record<UserRole, HomePageComponent>> = {
   [USER_ROLES.CUSTOMER]: CustomerHotelsPage,
-<<<<<<< support-property-manager-dashboard
   [USER_ROLES.PROPERTY_MANAGER]: PropertyManagerDashboardPage,
-=======
   [USER_ROLES.FRONT_DESK_STAFF]: FrontDeskDashboardPage,
->>>>>>> main
 }
 
 export const getHomePageComponent = (role?: UserRole): HomePageComponent =>
